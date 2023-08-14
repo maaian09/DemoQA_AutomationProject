ef27a47176de9e94cea6b7d6f50d4c3405c156fb
maaian09 <maaian.iancu@gmail.com>
2023-08-14 15:56:24 +0300

    optimizare2

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,13 +1,7 @@
-import constants.Constants;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
 
 public class BaseTest {
     WebDriver driver;
